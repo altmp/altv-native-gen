08a5e5be65e65f23b4c3c297bf8c7dc7985639f6
Alexander Schmid <schmid.alexander@outlook.de>
2021-10-22 15:56:10 +0200

    Optimize some native namespace calls

--- Durty.AltV.NativesTypingsGenerator/TypingDef/TypeDefFromNativeDbGenerator.cs
+++ Durty.AltV.NativesTypingsGenerator/TypingDef/TypeDefFromNativeDbGenerator.cs
@@ -43,60 +43,17 @@ namespace Durty.AltV.NativesTypingsGenerator.TypingDef
         public void AddFunctionsFromNativeDb(NativeDb nativeDb)
         {
             TypeDefModule nativesModule = _typeDefinition.Modules.First(m => m.Name == "natives");
-
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Graphics));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.System));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.App));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Audio));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Brain));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Cam));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Clock));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Cutscene));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Datafile));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Decorator));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Dlc));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Entity));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Event));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Files));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Fire));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Hud));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Interior));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Itemset));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Loadingscreen));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Localization));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Misc));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Mobile));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Money));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Netshopping));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Network));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Object));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Pad));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Pathfind));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Ped));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Physics));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Player));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Recording));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Replay));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Script));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Shapetest));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Socialclub));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Stats));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Streaming));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Task));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Vehicle));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Water));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Weapon));
-            nativesModule.Functions.AddRange(GetFunctionsFromNativeGroup(nativeDb.Zone));
+            nativesModule.Functions.AddRange(GetFunctionsFromNatives(nativeDb.AllNatives));
         }
 
-        private List<TypeDefFunction> GetFunctionsFromNativeGroup(Dictionary<string, Native> nativeGroup)
+        private List<TypeDefFunction> GetFunctionsFromNatives(List<Native> natives)
         {
             NativeTypeToTypingConverter nativeTypeToTypingConverter = new NativeTypeToTypingConverter();
             NativeReturnTypeToTypingConverter nativeReturnTypeToTypingConverter = new NativeReturnTypeToTypingConverter();
 
             //TODO: Add possibility to override certain properties of retrieved native function (for example override param type etc.)
             List<TypeDefFunction> functions = new List<TypeDefFunction>();
-            foreach (Native native in nativeGroup.Values.Where(native =>
+            foreach (Native native in natives.Where(native =>
                 native.AltFunctionName != string.Empty && native.Hashes != null && native.Hashes.Count != 0))
             {
                 int lastVar = native.Parameters.ToList().FindLastIndex(elem => !elem.IsReference);
